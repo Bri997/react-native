ccdad4295e8ee8c1846c4b46969179c0292c1246
brian <brian.musial@gmail.com>
2020-03-01 21:02:13 -0500

    added buttons for navigation

--- App.js
+++ App.js
@@ -2,16 +2,18 @@ import { createAppContainer } from 'react-navigation'
 import { createStackNavigator } from 'react-navigation-stack'
 import HomeScreen from './src/screens/HomeScreen'
 import ListScreen from './src/screens/ListScreen'
+import ComponentsScreen from './src/screens/ComponenetsScreen'
 
 const navigator = createStackNavigator(
   {
     Home: HomeScreen,
+    Components: ComponentsScreen,
     List: ListScreen
   },
   {
-    initialRouteName: 'List',
+    initialRouteName: 'Home',
     defaultNavigationOptions: {
-      title: 'App'
+      title: 'My App'
     }
   }
 )
--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -3,29 +3,31 @@ import { View, Text, StyleSheet, FlatList } from 'react-native'
 
 const ListScreen = () => {
   const friends = [
-    { name: 'Friend #1' },
-    { name: 'Friend #2' },
-    { name: 'Friend #3' },
-    { name: 'Friend #4' },
-    { name: 'Friend #5' },
-    { name: 'Friend #6' }
+    { name: 'Friend #1', age: 20 },
+    { name: 'Friend #2', age: 45 },
+    { name: 'Friend #3', age: 55 },
+    { name: 'Friend #4', age: 23 },
+    { name: 'Friend #5', age: 44 },
+    { name: 'Friend #6', age: 33 }
   ]
 
   return (
     <FlatList
-      horizontal
-      showsHorizontalScrollIndicator ={false}
       keyExtractor={friend => friend.name}
-        data={friends}
-        renderItem={({ item }) => {
-          return <Text style ={style.textStyle}>{item.name}</Text>
+      data={friends}
+      renderItem={({ item }) => {
+        return (
+          <Text style={style.textStyle}>
+            {item.name} - Age {item.age}
+          </Text>
+        )
       }}
     />
   )
 }
 
 const style = StyleSheet.create({
-  textStyle:{
+  textStyle: {
     marginVertical: 50
   }
 })
